6e44f5044107c2e6fb262e6d7c416a8f0499cb3f
isabellatc06 <180335105+isabellatc06@users.noreply.github.com>
2024-10-03 14:46:00 +0200

    login et enregistrement fonctionnel

--- app/views/auth.py
+++ app/views/auth.py
@@ -64,7 +64,8 @@ def login():
         # On récupère l'utilisateur avec le username spécifié (une contrainte dans la db indique que le nom d'utilisateur est unique)
         # La virgule après username est utilisée pour créer un tuple contenant une valeur unique
         user = db.execute('SELECT * FROM users WHERE username = ?', (username,)).fetchone()
-
+        print("BOUHHHHHHHHHHHH")
+        print(user)
         # On ferme la connexion à la base de données pour éviter les fuites de mémoire
         close_db()
 
@@ -80,7 +81,7 @@ def login():
         # De cette manière, à chaque requête de l'utilisateur, on pourra récupérer l'id dans le cookie session
         if error is None:
             session.clear()
-            session['user_id'] = user['id']
+            session['user_id'] = user['id_users']
             # On redirige l'utilisateur vers la page principale une fois qu'il s'est connecté
             return redirect("/")
         
@@ -119,7 +120,7 @@ def load_logged_in_user():
     else:
          # On récupère la base de données et on récupère l'utilisateur correspondant à l'id stocké dans le cookie session
         db = get_db()
-        g.user = db.execute('SELECT * FROM users WHERE id = ?', (user_id,)).fetchone()
+        g.user = db.execute('SELECT * FROM users WHERE id_users = ?', (user_id,)).fetchone()
         # On ferme la connexion à la base de données pour éviter les fuites de mémoire
         close_db()
 
